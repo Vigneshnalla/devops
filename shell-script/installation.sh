b7e55e33774700ebb25a55da7773a19a5ff4c12e
vignesh <vigneshnalla999@gmail.com>
2024-09-09 22:34:40 +0530

    -hello script

--- shell-script/installation.sh
+++ shell-script/installation.sh
@@ -7,12 +7,12 @@ USERID=$(id -u)
 echo $USERID;
 echo "user id printed"
 # # Check if the script is run as root (USERID 0 is root)
-# if [ $USERID -ne 0 ]; then
-#     echo "Please run this script with root access."
-#     exit 1
-# fi
+if [ $USERID -ne 0 ]; then
+    echo "Please run this script with root access."
+    exit 1
+fi
 
-# # Install MySQL using DNF
-# dnf install mysql -y
+# Install MySQL using DNF
+dnf install mysql -y
 
-# echo "is script proceding without erros"
+echo "is script proceding without erros"
