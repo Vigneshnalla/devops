2c536b2dc24873ad249099aae47ac504849656d7
vignesh <vigneshnalla999@gmail.com>
2024-09-10 22:20:52 +0530

    -hello script

--- shell-script/practice.sh
+++ shell-script/practice.sh
@@ -2,6 +2,25 @@
 
 echo "practice file"
 
-USERID=(id -u)
 
-echo "$USERID  "
+
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then
+    echo "Please run this script with root access."
+    exit 1 # manually exit if error comes.
+else
+    echo "You are super user."
+fi
+
+dnf install mysql -y
+
+
+if [$? -eq 0 ]
+then 
+    "my sql installation sucess"
+else
+    "dnf install mysql -y failure"
+fi
